6f9ce02216cdd36865a890208634bca5adedb743
Peppe-Quint <peppequintb@gmail.com>
2020-12-28 17:29:00 +0100

    feat(select): add style

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+import "./Form.css";
+
 const Form = () => {
   return (
     <form className="cases-filter form">
       <label htmlFor="" className="form-label" id="filter">
         Show me
       </label>
-      <select name="options" id="filter">
-        <option value="all">All</option>
+      <select className="form-select" name="options" id="filter">
+        <option value="all">all work</option>
         <option value="other">Option 1</option>
         <option value="nothing">Option 2</option>
       </select>
