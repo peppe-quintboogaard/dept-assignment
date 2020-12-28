04a5378a960257c27dbe0e31ba010bbe0f6cc221
Peppe-Quint <peppequintb@gmail.com>
2020-12-28 18:08:05 +0100

    feat(clients): add client style

--- src/components/ClientList/ClientList.js
+++ src/components/ClientList/ClientList.js
@@ -4,10 +4,14 @@ import Client from "./Client";
 
 const ClientList = () => {
   return (
-    <div className="client-list">
+    <section className="clients">
+      <h1 className="clients-title">Clients</h1>
+      <p className="clients-paragraph">
+        We value a great working relationship with our clients above all else. It’s why they often come to our parties. It’s also why we’re able to challenge and inspire them to reach for the stars.
+      </p>
       {/* map function to render all different cases from Case.js */}
       <Client />
-    </div>
+    </section>
   );
 };
 
