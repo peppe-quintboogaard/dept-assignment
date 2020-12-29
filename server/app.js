d79f918daf5f2d1b125781270f0370e28738a04d
Peppe-Quint <peppequintb@gmail.com>
2020-12-29 13:53:51 +0100

    feat(cases): fake json endpoint

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const mocker = require("connect-api-mocker");
 const port = 9000;
 const app = express();
 
-app.use("/api", mocker("mock-api"));
+app.use("/api", mocker("server"));
 
 console.log(`API is running on server port ${port}.`);
 app.listen(port);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React, { useEffect, useState } from "react";
 import { Cases, Clients, Footer, Header } from "./components";
 
-const App = () => (
-  <>
-    <Header />
-    <main className="container">
-      <Cases />
-      <Clients />
-    </main>
-    <Footer />
-  </>
-);
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url).then(async (res) => {
+      if (res.status !== 200) {
+        setData("Error");
+      }
+      const data = await res.json();
+      setData(data);
+    });
+  }, [setData, url]);
+
+  return [data];
+};
+
+const App = () => {
+  const [data] = useFetch("/data");
+  console.log(data);
+
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Cases />
+        <Clients />
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
