6e2e2190fbdcc2ccc0d7097221f447baf819d746
Peppe-Quint <peppequintb@gmail.com>
2020-12-29 11:31:32 +0100

    feat(api): fake rest api

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mocker = require("connect-api-mocker");
+
+const port = 9000;
+const app = express();
+
+app.use("/api", mocker("mock-api"));
+
+console.log(`API is running on server port ${port}.`);
+app.listen(port);
